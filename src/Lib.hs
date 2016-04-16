5c4ce1725f82f18d9584f21b26aec04c5638b9f8
Joshua Clayton <joshua.clayton@gmail.com>
2016-04-16 05:00:02 -0400

    Continue to extract to regex module

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,8 +8,7 @@ import Text.XML.HXT.Core
 import Text.XML.HXT.CSS
 import JArchiveParser.Request
 import JArchiveParser.Models
-
-import Text.Regex.Posix ((=~))
+import JArchiveParser.Regex
 
 someFunc :: IO ()
 someFunc = do
@@ -24,10 +23,11 @@ extractGames url = do
 extractGame :: ArrowXml cat => cat XmlTree Game
 extractGame = proc xml -> do
     url <- getAttrValue "href" -< xml
-    let gId = last $ head $ (url =~ "game_id=([0-9]+)" :: [[String]])
+    let gId = last $ head $ url `matchAllSubgroups` gameIdMatch
     returnA -< buildGame (toInt gId) url
   where
     toInt t = ((read t) :: Int)
+    gameIdMatch = mkRegex "game_id=([0-9]+)"
 
 textAtSelector :: ArrowXml cat => String -> cat XmlTree String
 textAtSelector selector =
