729a6cb641b6bd8b01b513e2d920fd05c45daec8
Joshua Clayton <joshua.clayton@gmail.com>
2016-04-17 21:17:17 -0400

    Bit of cleanup

--- src/JArchiveParser/Arrow/Util.hs
+++ src/JArchiveParser/Arrow/Util.hs
@@ -7,8 +7,11 @@ import Data.Tree.NTree.TypeDefs
 import Text.XML.HXT.Core
 
 arrToMaybe :: ArrowXml a => (b -> Bool) -> a XmlTree b -> a XmlTree (Maybe b)
-arrToMaybe f x =
-  (x >>> isA f >>> arr Just) `orElse` (constA Nothing)
+arrToMaybe p x =
+    (x >>> isA p >>> justA) `orElse` nothingA
+  where
+    justA = arr Just
+    nothingA = constA Nothing
 
 allText :: ArrowXml a => a (NTree XNode) String
 allText =
