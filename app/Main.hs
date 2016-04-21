fef328813c625c0863e360ef1dd4f6a59a37399d
Joshua Clayton <joshua.clayton@gmail.com>
2016-04-21 09:27:33 -0400

    Allow specification of request processor
    A developer may or may not want to handle requests in parallel, either
    for rate limiting concerns, computing performance, etc. This introduces
    processors for both sequential processing and parallel processing, and
    defaults to parallel processing within `defaultSettings`.

--- app/Main.hs
+++ app/Main.hs
@@ -5,17 +5,24 @@ import qualified JArchiveParser.GameParser as GP
 import JArchiveParser.Model
 import JArchiveParser.Cache
 import Control.Concurrent.ParallelIO
+import Control.Monad (void)
+
+data ExecutionProcessor a = ExecutionProcessor
+    { handleEach :: [IO a] -> IO [a]
+    , handleEnd :: IO ()
+    }
 
 data Settings = Settings
     { seasons :: Maybe [Int]
     , limit :: Maybe Int
     , seasonProcessor :: (Season -> IO ())
+    , executionProcessor :: ExecutionProcessor [Game]
     }
 
 main :: IO ()
 main = do
     mapM_ (downloadSeason defaultSettings) $ seasonsToDownload defaultSettings
-    stopGlobalPool
+    handleEnd $ executionProcessor defaultSettings
 
 downloadSeason :: Settings -> SeasonId -> IO ()
 downloadSeason settings sId = do
@@ -24,10 +31,12 @@ downloadSeason settings sId = do
     (seasonProcessor settings) season { games = concat games' }
   where
     extractGames gs =
-        parallel $ map extractGame $ (gamesToTake settings) gs
+        handleEach' $ map extractGame $ (gamesToTake settings) gs
     extractGame g = do
         putStrLn $ "Processing game " ++ (show $ gameId g)
         GP.someFunc $ gameId g
+    handleEach' =
+        (handleEach $ executionProcessor settings)
 
 gamesToTake :: Settings -> [a] -> [a]
 gamesToTake settings =
@@ -46,6 +55,16 @@ processSeason s = do
     cache s
     print s
 
+sequentialProcessor :: ExecutionProcessor [Game]
+sequentialProcessor =
+    ExecutionProcessor sequence handlerEnd
+  where
+    handlerEnd = return (void putStrLn "") :: IO ()
+
+parallelProcessor :: ExecutionProcessor [Game]
+parallelProcessor =
+    ExecutionProcessor parallel stopGlobalPool
+
 defaultSettings :: Settings
 defaultSettings =
-    Settings (Just [1]) Nothing processSeason
+    Settings (Just [1]) Nothing processSeason parallelProcessor
